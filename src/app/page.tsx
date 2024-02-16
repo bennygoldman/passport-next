3e58dad9f9e035ad9d783f24c72d9740d2fa2f96
bennygoldman <goldmanb@gmail.com>
2024-02-16 17:45:45 -0500

    Add Card Component, render data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,58 @@
-export default function Home() {
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from '../components/ui/card';
+
+interface Branch {
+  image: string;
+  BranchCode: string;
+  BranchName: string;
+  hasKidStuff: boolean;
+  Address: string;
+  NBHDName: string;
+  NBHDNo: number;
+  WardNo: number;
+  WardName: string;
+  id: string;
+}
+
+async function getBranches(): Promise<Branch[]> {
+  const result = await fetch('http://localhost:4000/branches');
+
+  return result.json();
+}
+
+export default async function Home() {
+  const branches = await getBranches();
+
   return (
     <main>
-      home
+      <div className='grid grid-cols-3 gap-8'>
+        {branches.map((branch) => (
+          <Card key={branch.id} className='flex flex-col justify-between'>
+            <CardHeader className='flex-row gap-4 items-center'>
+              {/* image to go here */}
+              <div>
+                <CardTitle>{branch.BranchName}</CardTitle>
+                <CardDescription>{branch.NBHDName}</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{branch.Address}</p>
+            </CardContent>
+            <CardFooter className='flex justify-between'>
+              <button>View Branch</button>
+              {branch.hasKidStuff && (
+                <p className=' bg-green-400 bg-opacity-70'>Has Kid Stuff!</p>
+              )}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </main>
-  )
+  );
 }
